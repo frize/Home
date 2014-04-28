fed10735ea64fb9ae4d0af2c7a3dd38007ee1807
Quang Thai <quangthaiptit@gmail.com>
2014-04-28 08:45:17 +0700

    Create Archer Project

---
+++ ArcherGame/ArcherGame/Classes/AppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.h
+//  ArcherGame
+//
+//  Created by tico on 4/28/14.
+//  Copyright ISB 2014. All rights reserved.
+//
+// -----------------------------------------------------------------------
+
+#import "cocos2d.h"
+
+@interface AppDelegate : CCAppDelegate
+@end
---
+++ ArcherGame/ArcherGame/Supporting Files/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ArcherGame
+//
+//  Created by tico on 4/28/14.
+//  Copyright ISB 2014. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    @autoreleasepool {
+        int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+        return retVal;
+    }
+}
